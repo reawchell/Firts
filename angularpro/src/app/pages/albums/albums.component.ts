1c83266231b5d44c910edd41750a19239fee1891
i <85507670+reawchell@users.noreply.github.com>
2023-04-27 11:25:46 +0200

    conect

--- angularpro/src/app/pages/albums/albums.component.ts
+++ angularpro/src/app/pages/albums/albums.component.ts
@@ -10,14 +10,28 @@ import { ApiService } from "src/app/services/api.service";
 })
 export class AlbumsComponent {
   listaRap: Cantantes []= []
-constructor(private rapService: ApiService){}
+  listaBosaNova: Cantantes [] = []
+  listaPop: Cantantes [] = []
+
+constructor(private rapService: ApiService, private bossaService: ApiService, private popService: ApiService){}
+
 
 ngOnInit() :void {
   this.rapService.getRAP().subscribe((data:any)=>{
     console.log(data);
 
     this.listaRap= [...data]
-
   })
+  
+  this.bossaService.getBOSSANOVA().subscribe((data1:any)=>{
+  console.log(data1)
+  this.listaBosaNova= [...data1]
+})
+
+this.popService.getPOP().subscribe((data2:any)=>{
+  console.log(data2)
+  this.listaPop= [...data2]
+})
+
 }
 }
--- angularpro/src/app/services/api.service.ts
+++ angularpro/src/app/services/api.service.ts
@@ -6,10 +6,20 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
   baseUrl:string = "http://localhost:3000/RAP"
+  baseUrl2:string = "http://localhost:3000/BOSSANOVA"
+  baseUrl3:string = "http://localhost:3000/POP"
+  
+  
 
 
   constructor(private http: HttpClient) {}
   getRAP = () => {
-    return this.http.get(this.baseUrl)
+    return this.http.get(this.baseUrl) 
+  }
+  getBOSSANOVA = () => {
+    return this.http.get(this.baseUrl2)
+  }
+  getPOP = () => {
+    return this.http.get(this.baseUrl3)
   }
 }
