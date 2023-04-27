9e380df809724985cd0e2ebd664e00df99227483
diegosanchez96 <diego.sanchez@bootcamp-upgrade.com>
2023-04-27 09:35:14 +0200

    desarrollado

--- angularpro/src/app/app-routing.module.ts
+++ angularpro/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './pages/home/home.component';
 const routes: Routes = [
   {path:"", component: HomeComponent},
   {path:"albums", component: AlbumsComponent},
-  {path:"formulario", component: FormulariosComponent},
+  {path:"formularios", component: FormulariosComponent},
 ];
 
 
